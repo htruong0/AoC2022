f7877e95dbc2c725cf92c06871cd7fd25a7ec30d
Henry Truong <henry.truong@durham.ac.uk>
2022-12-02 16:33:37 +0000

    attempt 2 for day 2

--- day2/solution.jl
+++ day2/solution.jl
@@ -30,7 +30,69 @@ scores_1 = getindex.(Ref(rules_1), rounds)
 total_score_1 = sum(scores_1)
 println("Total score for part 1 = $(total_score_1)")
 
-# part 1
+# part 2
 scores_2 = getindex.(Ref(rules_2), rounds)
 total_score_2 = sum(scores_2)
 println("Total score for part 1 = $(total_score_2)")
+
+###### attempt 2 ######
+
+@enum ShapeScore R=1 P=2 S=3
+
+struct Shape
+    shape::ShapeScore
+    winAgainst::ShapeScore
+    loseAgainst::ShapeScore
+end
+
+function get_score_pt1(round)
+    opponentPick, playerPick = getindex.(Ref(convertToShape), split(round, " "))
+    score = Int(playerPick.shape)
+    if playerPick.winAgainst === opponentPick.shape
+        score += 6
+    elseif playerPick.shape === opponentPick.shape
+        score += 3
+    elseif playerPick.loseAgainst === opponentPick.shape
+        score += 0
+    end
+end
+
+function get_score_pt2(round)
+    opponentPick, playerResult = getindex.(Ref(convertToResults), split(round, " "))
+    score = playerResult
+    if playerResult === 0
+        score += Int(opponentPick.winAgainst)
+    elseif playerResult === 3
+        score += Int(opponentPick.shape)
+    elseif playerResult === 6
+        score += Int(opponentPick.loseAgainst)
+    end
+end
+
+rock = Shape(R, S, P)
+paper = Shape(P, R, S)
+scissors = Shape(S, P, R)
+
+convertToShape = Dict(
+    "A" => rock,
+    "B" => paper,
+    "C" => scissors,
+    "X" => rock,
+    "Y" => paper,
+    "Z" => scissors,
+)
+
+convertToResults = Dict(
+    "A" => rock,
+    "B" => paper,
+    "C" => scissors,
+    "X" => 0,
+    "Y" => 3,
+    "Z" => 6
+)
+
+score_pt1 = get_score_pt1.(rounds) |> sum
+println("Score for part 1 = $(score_pt1)")
+
+score_pt2 = get_score_pt2.(rounds) |> sum
+println("Score for part 2 = $(score_pt2)")
